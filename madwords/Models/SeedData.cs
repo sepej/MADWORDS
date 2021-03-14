035bea6f64a8e653435139f9656088900f025a8a
jasii <josephsepe@gmail.com>
2021-03-14 13:05:12 -0700

    Finished beta

--- madwords/Models/SeedData.cs
+++ madwords/Models/SeedData.cs
@@ -19,7 +19,7 @@ namespace madwords.Models
                 AppUser siteadmin = new AppUser
                 {
                     UserName = "SiteAdmin",
-                    Name = "Site Admin"
+                    Name = "SiteAdmin"
                 };
                 userManager.CreateAsync(siteadmin, "Secret-123").Wait();
                 IdentityRole adminRole = roleManager.FindByNameAsync("Admin").Result;
@@ -54,7 +54,55 @@ namespace madwords.Models
                 };
                 context.BlogPosts.Add(post);
 
-                context.SaveChanges(); // stores all the reviews in the DB
+                MadwordTemplate template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Smash Mouth",
+                    MadwordTemplateText = "Somebody once told me the[Noun] is gonna[Verb] me, I ain't the [Adjective] [Noun] in the [Place] , She was looking kind of [Adjective] with her [Noun] and her [Noun] in the shape of a [Letter] on her [Noun]",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Rudolph",
+                    MadwordTemplateText = "[Name] the [Adjective] -nosed [Noun] had a very [Adjective] nose, And if you ever [Verb] it, You would even say it glows, All of the other [Noun] used to [Verb] and call him [Noun] , They never let [Adjective] [Name] [Verb] in any [Noun] games.",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Wild Animal",
+                    MadwordTemplateText = "The majestic [Animal] has roamed the forests of [Country] for thousands of years. Today, it wanders in search of [Noun] . It must find [Noun] to survive the harsh [Season]",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Sk8terboi",
+                    MadwordTemplateText = "He was a [Noun] , She was a [Noun] . Can I make it any more obvious? He was a [Noun] , She did [Sport] . What more can I say? He wanted [Noun] , She'd never tell. Secretly she [Verb_(past_tense)] [Noun] as well. But all of her [Noun] , Stuck up their [Noun] , They had a problem with his [Adjective] [Noun_(plural)]",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Blink182",
+                    MadwordTemplateText = "All the [Adjective] [Noun_(plural)] , true care truth brings. I'll [Verb] one [Noun] . Your [Noun] , [Adjective] [Noun] . Always I know, you'll be at my [Noun] . [Verb_(ending_w/_-ing)] , [Verb_(ending_w/_-ing)] , [Verb_(ending_w/_-ing)]",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                template = new MadwordTemplate
+                {
+                    MadwordTemplateTitle = "Third Eye Blind - Jumper",
+                    MadwordTemplateText = "I wish you would [Verb] back from that [Noun] my friend. You could [Verb] with all the [Noun_(plural)] that you've been [Verb_(ending_w/_-ing)] in. And if you do not want to [Verb] me again, I would understand, I would understand.",
+                    MadwordTemplateDate = DateTime.Now,
+                };
+                context.MadwordTemplates.Add(template);
+
+                context.SaveChanges(); // stores all in the DB
             }
         }
     }
